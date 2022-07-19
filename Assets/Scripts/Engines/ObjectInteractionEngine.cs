caf9538025d13fec0fb0b4cfcdfa808484425140
BlizzardBunny <67761993+BlizzardBunny@users.noreply.github.com>
2022-07-20 02:52:22 +0800

    dialogue function complete

---
+++ Assets/Scripts/Engines/ObjectInteractionEngine.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectInteractionEngine : MonoBehaviour
+{
+    #region Object References
+
+    [SerializeField] private Animator playerAnim;
+
+    public Canvas dialogueCanvas;
+    [SerializeField] private Image npcPic;
+    [SerializeField] private TMPro.TextMeshProUGUI npcName;
+    [SerializeField] private TMPro.TextMeshProUGUI npcDialogue;
+    [SerializeField] private Button nextLine;
+
+    #endregion
+
+    #region Variables
+    private GameObject obj;
+    private NPCDialogueInfo dialogueInfo;
+    private int dialogueIndex = 1;
+
+    //Directions
+    private Vector3 up = new Vector3(0.0f, 1.0f, 0.0f);
+    private Vector3 left = new Vector3(-1.0f, 0.0f, 0.0f);
+    private Vector3 down = new Vector3(0.0f, -1.0f, 0.0f);
+    private Vector3 right = new Vector3(1.0f, 0.0f, 0.0f);
+
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextLine.onClick.AddListener(ContDialogue);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CheckObject(RaycastHit2D hit)
+    {      
+        if (hit)
+        {
+            Debug.Log("Wa");
+            obj = hit.transform.gameObject;
+
+            if (hit.transform.tag == "NPC")
+            {
+                if (!dialogueCanvas.enabled)
+                {
+                    StartDialogue();
+                }
+            }
+        }
+    }
+
+    public void StartDialogue()
+    {
+        dialogueCanvas.enabled = true;
+
+        dialogueInfo = obj.GetComponent<NPCDialogueInfo>();
+
+        npcDialogue.text = dialogueInfo.dialogue[0];
+        npcName.text = dialogueInfo.names[0];
+        npcPic.sprite = dialogueInfo.sprites[0];
+    }
+
+    void ContDialogue()
+    {
+        if (dialogueIndex < dialogueInfo.dialogue.Length)
+        {
+            npcDialogue.text = dialogueInfo.dialogue[dialogueIndex];
+            npcName.text = dialogueInfo.names[dialogueIndex];
+            npcPic.sprite = dialogueInfo.sprites[dialogueIndex];
+
+            dialogueIndex++;
+        }
+        else
+        {
+            dialogueIndex = 1;
+            dialogueCanvas.enabled = false;
+        }
+    }
+}
