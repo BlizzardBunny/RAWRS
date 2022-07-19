9379637214ebf5772e1340b2a28af11fe770c6ce
BlizzardBunny <67761993+BlizzardBunny@users.noreply.github.com>
2022-07-19 21:08:58 +0800

    movement complete

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,6 +10,17 @@ public class PlayerMovement : MonoBehaviour
 
     #endregion
 
+    #region Variables
+    public float moveSpeed = 5.0f;
+
+    //Directions
+    private Vector3 up = new Vector3(0.0f, 1.0f, 0.0f);
+    private Vector3 left = new Vector3(-1.0f, 0.0f, 0.0f);
+    private Vector3 down = new Vector3(0.0f, -1.0f, 0.0f);
+    private Vector3 right = new Vector3(1.0f, 0.0f, 0.0f);
+
+    #endregion
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,21 +30,25 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKey(KeyCode.W))
         {
             playerAnim.SetInteger("direction", 0);
+            Move(up);
         }
-        else if (Input.GetKeyDown(KeyCode.A))
+        else if (Input.GetKey(KeyCode.A))
         {
             playerAnim.SetInteger("direction", 1);
+            Move(left);
         }
-        else if (Input.GetKeyDown(KeyCode.S))
+        else if (Input.GetKey(KeyCode.S))
         {
             playerAnim.SetInteger("direction", 2);
+            Move(down);
         }
-        else if (Input.GetKeyDown(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D))
         {
             playerAnim.SetInteger("direction", 3);
+            Move(right);
         }
 
         if (!playerAnim.GetBool("isRunning"))
@@ -44,6 +59,18 @@ public class PlayerMovement : MonoBehaviour
         if (!Input.anyKey)
         {
             playerAnim.SetBool("isRunning", false);
+        }
+    }
+
+    void Move(Vector3 direction)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.TransformDirection(direction), 0.5f);
+        Debug.Log((bool)hit);
+        Debug.DrawRay(transform.position, direction*0.5f, Color.red);
+
+        if (!hit)
+        {     
+            transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, moveSpeed * Time.deltaTime);
         }
     }
 }
