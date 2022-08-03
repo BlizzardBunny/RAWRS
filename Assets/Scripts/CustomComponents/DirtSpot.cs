06c7d91da1c7e336496ee7241f3a7ce282e219f5
BlizzardBunny <67761993+BlizzardBunny@users.noreply.github.com>
2022-08-03 16:56:08 +0800

    bathing task setup

---
+++ Assets/Scripts/CustomComponents/DirtSpot.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DirtSpot : MonoBehaviour
+{
+    [SerializeField] private Canvas canvas;
+
+    private void OnMouseEnter()
+    {
+        Debug.Log("boop");
+    }
+}
