843275f74d58a838699b75266ab593a94466fe99
BlizzardBunny <67761993+BlizzardBunny@users.noreply.github.com>
2022-09-19 14:31:17 +0800

    fixed water, TODO: add confetti as complete feedback

--- Assets/Scripts/CustomComponents/BowlState.cs
+++ Assets/Scripts/CustomComponents/BowlState.cs
@@ -4,23 +4,39 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class BowlState : MonoBehaviour, IPointerEnterHandler
+public class BowlState : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {    
     [SerializeField] private int neededTool;
     [SerializeField] private CanvasGroup canvas;
     [SerializeField] private Canvas dirtCanvas;
+    private bool isPouring;
 
     public void OnPointerEnter(PointerEventData eventData)
     {
         if (TaskEngine.tool == neededTool && !dirtCanvas.enabled)
         {            
+            if (TaskEngine.tool == 4)
+            {
+                isPouring = true;
+            }
+
             canvas.alpha -= 0.1f;
         }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        isPouring = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isPouring)
+        {
+            canvas.alpha -= 0.5f * Time.deltaTime;
+        }
+
         if (canvas.alpha <= 0)
         {
             Destroy(gameObject);
