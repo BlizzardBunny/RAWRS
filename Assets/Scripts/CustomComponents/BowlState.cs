46632d2406de7138dd95e89ca341a852878332f3
BlizzardBunny <67761993+BlizzardBunny@users.noreply.github.com>
2022-10-05 16:59:50 +0800

    trying to figure out the wateranims
    Currently: stuck trying to play different anims in sequence

--- Assets/Scripts/CustomComponents/BowlState.cs
+++ Assets/Scripts/CustomComponents/BowlState.cs
@@ -4,58 +4,40 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class BowlState : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class BowlState : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler
 {    
     [SerializeField] private int neededTool;
+    [SerializeField] private bool isPouring;
     [SerializeField] private CanvasGroup canvas;
     [SerializeField] private Canvas dirtCanvas;
-    [SerializeField] private Animator waterAnim;
-    private bool isPouring;
+    [SerializeField] private Animation particleAnim;
+
 
     public void OnPointerEnter(PointerEventData eventData)
     {
         if (TaskEngine.tool == neededTool && !dirtCanvas.enabled)
         {            
-            if (TaskEngine.tool == 4)
-            {
-                isPouring = true;
-                if (waterAnim != null)
-                {
-                    waterAnim.SetBool("isPouring", true);
-                }
-            }
-            else
+            if (!isPouring)
             {
                 canvas.alpha -= 0.2f;
             }
         }
     }
-
-    public void OnPointerExit(PointerEventData eventData)
+    public void OnPointerClick(PointerEventData eventData)
     {
-        isPouring = false;
-        if (waterAnim != null)
+        if (isPouring)
         {
-            waterAnim.SetBool("isPouring", false);
+            //Queues each of these animations to be played one after the other
+            particleAnim.PlayQueued("WaterCursor", QueueMode.CompleteOthers);
+            particleAnim.PlayQueued("WaterBall", QueueMode.CompleteOthers);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isPouring)
-        {
-            canvas.alpha -= 0.5f * Time.deltaTime;
-        }
-
         if (canvas.alpha <= 0)
         {
-            isPouring = false;
-            if (waterAnim != null)
-            {
-                waterAnim.SetBool("isPouring", false);
-            }
-
             if (this.name == "FoodBowl (1)" || this.name == "WaterBowl (1)")
             {
                 Cursor.visible = true;
