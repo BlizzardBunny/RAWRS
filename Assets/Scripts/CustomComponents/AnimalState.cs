622de6c2773fe62942f4f49d23f8fe1fbec0b03b
BlizzardBunny <67761993+BlizzardBunny@users.noreply.github.com>
2023-01-24 16:56:35 +0800

    added tool slider functionality in bath task

--- Assets/Scripts/CustomComponents/AnimalState.cs
+++ Assets/Scripts/CustomComponents/AnimalState.cs
@@ -1,25 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class AnimalState : MonoBehaviour, IPointerEnterHandler
+public class AnimalState : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {    
     [SerializeField] private int neededTool;
     [SerializeField] CanvasGroup canvas;
     [SerializeField] Canvas dirtCanvas;
+    [SerializeField] Slider stressSlider, toolSlider;
+    private bool isActive;
 
     public void OnPointerEnter(PointerEventData eventData)
     {
         if (TaskEngine.tool == neededTool && !dirtCanvas.enabled)
         {
-            canvas.alpha -= 0.05f;
+            isActive = true;
         }
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        isActive = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isActive)
+        {
+            canvas.alpha -= 0.25f * Time.deltaTime;
+            toolSlider.value = 1 - canvas.alpha;
+            stressSlider.value += 0.75f * Time.deltaTime;
+        }
+
         if (canvas.alpha <= 0)
         {
             dirtCanvas.enabled = true;
