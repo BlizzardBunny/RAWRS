ecf5cbd7b4a3b9698e52f5cdaad33f6e6e84addd
BlizzardBunny <67761993+BlizzardBunny@users.noreply.github.com>
2023-04-10 22:14:24 +0800

    added tool controls

--- Assets/Scripts/Engines/TaskEngine.cs
+++ Assets/Scripts/Engines/TaskEngine.cs
@@ -38,7 +38,7 @@ public class TaskEngine : MonoBehaviour
     public static int checkUpTasksTodo = 3;
 
     public static int tool = -1;
-    public static int taskType = 0;
+    public static int taskType = 3;
     public static bool petType = true;
     public static int currStationID = -1;
     #endregion
