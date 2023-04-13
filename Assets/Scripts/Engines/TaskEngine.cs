d18a5e4d62c46e6fb5d95659561772dfe5173ea0
BlizzardBunny <67761993+BlizzardBunny@users.noreply.github.com>
2023-04-14 00:16:40 +0800

    update Bathing task #91

--- Assets/Scripts/Engines/TaskEngine.cs
+++ Assets/Scripts/Engines/TaskEngine.cs
@@ -38,8 +38,8 @@ public class TaskEngine : MonoBehaviour
     public static int checkUpTasksTodo = 3;
 
     public static int tool = -1;
-    public static int taskType;
-    public static bool petType;
+    public static int taskType = 0;
+    public static bool petType = false;
     public static int currStationID = -1;
     #endregion
 
